887b099eb6a2ceac85e4db2318174981a796ace0
Nazar Pyskor <pyskornazik@gmail.com>
2026-08-25 03:38:54 +0300

    Redesign Author model and configure Author admin

--- library/author/admin.py
+++ library/author/admin.py
@@ -1,31 +1,11 @@
 from django.contrib import admin
-
 from .models import Author
 
 
+@admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
-    list_display = ["name", "surname", "get_books"]
-
-    def get_books(self, obj):
-        return ", ".join([f"{book.name}" for book in obj.books.all()])
-
-    get_books.short_description = "Books"
-    search_fields = ["name", "surname"]
-    list_filter = ["books"]
-    readonly_fields = ["get_books"]
-    fieldsets = [
-        (
-            "Personal info",
-            {
-                "fields": [
-                    "name",
-                    "surname",
-                    "patronymic",
-                ],
-            },
-        ),
-        (None, {"fields": ["get_books"]}),
-    ]
-
-
-admin.site.register(Author, AuthorAdmin)
+    list_display = ["first_name", "last_name", "country"]
+    list_filter = ["country"]
+    search_fields = ["first_name", "last_name"]
+    ordering = ["last_name", "first_name"]
+    fields = ["first_name", "last_name", "country"]
