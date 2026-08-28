fa46c2d16f9dedc757b67860340f914bf81f5adf
Nazar Pyskor <pyskornazik@gmail.com>
2026-08-28 12:52:30 +0300

    Сlean up Book model and configure BookAdmin

--- library/book/admin.py
+++ library/book/admin.py
@@ -1,45 +1,33 @@
-# from django.contrib import admin
-
-# from .models import Book
-
-
-# class BookAdmin(admin.ModelAdmin):
-#     list_display = ["name", "get_authors", "count", "get_users"]
-
-#     def get_authors(self, obj):
-#         return ", ".join(
-#             [f"{author.name} {author.surname}" for author in obj.authors.all()]
-#         )
-
-#     def get_users(self, obj):
-#         return ", ".join(
-#             [f"{user.first_name} {user.last_name}" for user in obj.users.all()]
-#         )
-
-#     get_authors.short_description = "Authors"
-#     get_users.short_description = "Borrowed by"
-#     list_filter = ["name", "count", "authors", "users"]
-#     readonly_fields = ["get_authors", "get_users"]
-#     fieldsets = [
-#         (
-#             None,
-#             {
-#                 "fields": [
-#                     "name",
-#                     "get_authors",
-#                     "description",
-#                 ],
-#             },
-#         ),
-#         (
-#             "Borrowing Information",
-#             {
-#                 "classes": ["collapse"],
-#                 "fields": ["get_users", "count"],
-#             },
-#         ),
-#     ]
-#     search_fields = ["name"]
+from django.contrib import admin
+from .models import Book
 
 
-# admin.site.register(Book, BookAdmin)
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    """Admin configuration for the Book model."""
+
+    list_display = ["title", "author", "count", "is_available_display"]
+    list_filter = ["author"]
+    search_fields = ["title", "author__first_name", "author__last_name"]
+    list_select_related = ["author"]
+    autocomplete_fields = ["author"]
+    ordering = ["title"]
+
+    fieldsets = (
+        (
+            "Book Details",
+            {
+                "fields": (
+                    "title",
+                    "author",
+                    "count",
+                    "description",
+                ),
+            },
+        ),
+    )
+
+    @admin.display(boolean=True, description="Available")
+    def is_available_display(self, obj):
+        """Display stock availability as a boolean icon."""
+        return obj.is_available
--- library/book/models.py
+++ library/book/models.py
@@ -1,147 +1,20 @@
 from django.db import models
-from authentication.models import User
+from author.models import Author
 
 
 class Book(models.Model):
-    """
-    This class represents an Author. \n
-    Attributes:
-    -----------
-    param name: Describes name of the book
-    type name: str max_length=128
-    param description: Describes description of the book
-    type description: str
-    param count: Describes count of the book
-    type count: int default=10
-    param authors: list of Authors
-    type authors: list->Author
-    """
+    """Represents a book available in the library catalog."""
 
-    name = models.CharField(blank=True, max_length=128)
-    description = models.CharField(blank=True, max_length=256)
-    count = models.IntegerField(default=10)
-    users = models.ManyToManyField(User, null=True, related_name="books")
-    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=128)
+    description = models.CharField(max_length=256, blank=True, null=True)
+    author = models.ForeignKey(Author, on_delete=models.PROTECT, related_name="books")
+    count = models.PositiveIntegerField(default=1)
 
     def __str__(self):
-        """
-        Magic method is redefined to show all information about Book.
-        :return: book id, book name, book description, book count, book authors
-        """
-        # return f"'id': {self.id}, 'name': '{self.name}', 'description': '{self.description}', 'count': {self.count}, 'authors': {[author.id for author in self.authors.all()]}"
-        return f"{self.name}"
+        """String representation of the Book object"""
+        return f"{self.title} ({self.author.last_name})"
 
-    def __repr__(self):
-        """
-        This magic method is redefined to show class and id of Book object.
-        :return: class, id
-        """
-        return f"Book(id={self.id})"
-
-    @staticmethod
-    def get_by_id(book_id):
-        """
-        :param book_id: SERIAL: the id of a Book to be found in the DB
-        :return: book object or None if a book with such ID does not exist
-        """
-        return Book.objects.get(id=book_id) if Book.objects.filter(id=book_id) else None
-
-    @staticmethod
-    def delete_by_id(book_id):
-        """
-        :param book_id: an id of a book to be deleted
-        :type book_id: int
-        :return: True if object existed in the db and was removed or False if it didn't exist
-        """
-        if Book.get_by_id(book_id) is None:
-            return False
-        Book.objects.get(id=book_id).delete()
-        return True
-
-    @staticmethod
-    def create(name, description, count=10, authors=None):
-        """
-        param name: Describes name of the book
-        type name: str max_length=128
-        param description: Describes description of the book
-        type description: str
-        param count: Describes count of the book
-        type count: int default=10
-        param authors: list of Authors
-        type authors: list->Author
-        :return: a new book object which is also written into the DB
-        """
-        if len(name) > 128:
-            return None
-
-        book = Book()
-        book.name = name
-        book.description = description
-        book.count = count
-        if authors is not None:
-            for elem in authors:
-                book.authors.add(elem)
-        book.save()
-        return book
-
-    def to_dict(self):
-        """
-        :return: book id, book name, book description, book count, book authors
-        :Example:
-        | {
-        |   'id': 8,
-        |   'name': 'django book',
-        |   'description': 'bla bla bla',
-        |   'count': 10',
-        |   'authors': []
-        | }
-        """
-
-    def update(self, name=None, description=None, count=None):
-        """
-        Updates book in the database with the specified parameters.\n
-        param name: Describes name of the book
-        type name: str max_length=128
-        param description: Describes description of the book
-        type description: str
-        param count: Describes count of the book
-        type count: int default=10
-        :return: None
-        """
-        if name is not None:
-            self.name = name
-
-        if description is not None:
-            self.description = description
-
-        if count is not None:
-            self.count = count
-
-        self.save()
-
-    def add_authors(self, authors):
-        """
-        Add  authors to  book in the database with the specified parameters.\n
-        param authors: list authors
-        :return: None
-        """
-        if authors is not None:
-            for elem in authors:
-                self.authors.add(elem)
-                self.save()
-
-    def remove_authors(self, authors):
-        """
-        Remove authors to  book in the database with the specified parameters.\n
-        param authors: list authors
-        :return: None
-        """
-        for elem in self.authors.values():
-            self.authors.remove(elem["id"])
-
-    @staticmethod
-    def get_all():
-        """
-        returns data for json request with QuerySet of all books
-        """
-        return list(Book.objects.all())
+    @property
+    def is_available(self):
+        """Check if at least one copy of the book is in stock."""
+        return self.count > 0
